ef828bb4f6758b3e038ee73b73fe446eb4776078
むずでょ <muzudho1@gmail.com>
2021-09-18 00:33:20 +0900

    IsHitTestVisible

---
+++ KifuwarabeShogiBoard/KifuwarabeShogiBoard/Views/MainWindowView.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace KifuwarabeShogiBoard.Views
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for MainWindowView.xaml
+    /// </summary>
+    public partial class MainWindowView : Window
+    {
+        public MainWindowView()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            // ウィンドウを閉じます
+            this.Close();
+        }
+    }
+}
